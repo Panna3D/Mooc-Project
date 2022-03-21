bff132e7c2585930f46553cd648d9ee18851426a
3D <noproblem2602@gmail.com>
2022-03-21 12:08:20 +0700

    add new function [add to card]

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
-import Products from "./components/Product/Products";
-import Header from "./components/Layout/Header";
-import Carts from "./components/Cart/Cart";
+import { Fragment, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import {useState} from 'react';
-import { useSelector} from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import FilterCategory from './components/Shop/FilterCategory';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+// let isInitial = true;
 
 function App() {
-// Input store, actions, slices
-const showCart = useSelector(state => state.ui.cartIsVisible); 
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
-//----------------FUNCTIONS Filter-----------------------
-const [inputText, setInputText] = useState(""); // SearchBox state
+  // SearchBox
+  const [inputText, setInputText] = useState(""); // SearchBox state
+  // DropdownList
+  const [filterCategory, setFilterCategory] = useState(false); // SearchBox state
 
-let inputHandler = (e) => {
-  //convert input text to lower case
-  var lowerCase = e.target.value.toLowerCase();
-  setInputText(lowerCase);
-  // console.log(lowerCase);
-};
+  let isSearchBox = false;
 
+  let inputHandler = (e) => {
+    isSearchBox = true; // check is search?
+    let lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+    console.log(lowerCase);
+  };
+
+  // --------- API
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    // if (isInitial) {
+    //   isInitial = false;
+    //   return;
+    // }
+
+    // if (cart.changed) {
+      dispatch(sendCartData(cart));
+    // }
+  }, [cart, dispatch]);
+
+  // --------- Render
   return (
-    <main>
-      <Header/>
-      <div>
-        { showCart && <Carts/>}
-       <h1>Search</h1>
-        <div>
-          <input
-          id="outlined-basic"
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+        <input
+          placeholder="Search bar"
           onChange={inputHandler}
+          variant="outlined"
           label="Search"
+        />
+
+      <div>
+        <FilterCategory />
+      </div>          
+
+      <Layout>
+        {showCart && <Cart />}
+          {/* {filterCategory && <Products 
+            input = {inputText}
           />
-        </div>
-      </div>
-      <Products input={inputText}/>
-    </main>
+          } */}
+          <Products 
+            input = {inputText}
+          />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import store from './store/reducer';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from '../src/components/Store/index.js';
 
-// Declear provider for high level component to use Redux
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
